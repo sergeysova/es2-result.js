23c5eb4731b4123bba9ad61843192e26e747b3ed
Sergey Sova <mail@sergeysova.com>
2018-04-04 13:02:54 +0300

    refactor: to functions instead of classes

--- test/test.js
+++ test/test.js
@@ -5,59 +5,27 @@ import test from 'ava'
 import { Result, Ok, Err } from '../lib'
 
 
-test('Ok and Err extends Result', (t) => {
-  t.true(new Result.Ok() instanceof Result, 'Ok instanceof Result')
-  t.true(new Result.Err() instanceof Result, 'Err instanceof Result')
-})
-
 test('exported Ok and Err is equal Result static props', (t) => {
   t.is(Err, Result.Err)
   t.is(Ok, Result.Ok)
 })
 
-test('constructor of Result return Err or Ok', (t) => {
-  t.true(new Result(0) instanceof Result.Ok, 'Result(number) return Ok')
-  t.true(new Result(new Error('foo')) instanceof Result.Err, 'Result(Error) return Err')
-  t.true(new Result(new TypeError('foo')) instanceof Result.Err, 'Result(TypeError) return Err')
-  t.true(new Result(new ReferenceError('foo')) instanceof Result.Err, 'Result(ReferenceError) return Err')
+test('no constructor of Result', (t) => {
+  t.true(typeof Result === 'object')
 })
 
-test('Result.from equals constructor', (t) => {
-  t.true(Result.of(0) instanceof Result.Ok, 'Result(number) return Ok')
-  t.true(Result.of(new Error('foo')) instanceof Result.Err, 'Result(Error) return Err')
-  t.true(Result.of(new TypeError('foo')) instanceof Result.Err, 'Result(TypeError) return Err')
-  t.true(Result.of(new ReferenceError('foo')) instanceof Result.Err, 'Result(ReferenceError) return Err')
+test('Ok()', (t) => {
+  t.is(Result.Ok(1).unwrap(), 1)
 })
 
-test('Ok.from', (t) => {
-  t.true(Result.Ok.of(1) instanceof Result.Ok)
-  t.true(Result.Ok.of(1) instanceof Result)
-  t.is(Result.Ok.of(1)._data, 1)
+test('Err()', (t) => {
+  t.is(Result.Err(1).unwrapErr(), 1)
 })
 
-test('Err.from', (t) => {
-  t.true(Result.Err.of(1) instanceof Result.Err)
-  t.true(Result.Err.of(1) instanceof Result)
-  t.is(Result.Err.of(1)._error, 1)
-})
-
-test('Result.ok', (t) => {
-  t.deepEqual(Result.ok(1), Ok.of(1))
-  t.deepEqual(Result.ok(Result.ok(1)), Ok.of(Ok.of(1)))
-})
-
-test('Result.err', (t) => {
-  t.deepEqual(Result.err(1), Err.of(1))
-  t.deepEqual(Result.err(Result.err(1)), Err.of(Err.of(1)))
-})
 
 test('Result.isResult(target)', (t) => {
-  t.true(Result.isResult(Ok.of(1)))
-  t.true(Result.isResult(Err.of(1)))
-  t.true(Result.isResult(Result.of(1)))
-  t.true(Result.isResult(Result.of(new Error('Foo'))))
-
-  t.true(Result.isResult(new (class extends Result.Ok {})()))
+  t.true(Result.isResult(Ok(1)))
+  t.true(Result.isResult(Err(1)))
 
   t.false(Result.isResult(new Error('Bar')))
   t.false(Result.isResult(12))
@@ -67,167 +35,160 @@ test('Result.isResult(target)', (t) => {
 })
 
 test('isOk()', (t) => {
-  t.true(Result.of(0).isOk())
-  t.false(Result.of(new Error('foo')).isOk())
-  t.true(Ok.isOk(Result.of(1)))
-  t.false(Err.isErr(Result.of(1)))
+  t.true(Ok(0).isOk())
+  t.false(Err(new Error('foo')).isOk())
+  t.true(Ok.isOk(Ok(1)))
+  t.false(Err.isErr(Ok(1)))
 })
 
 test('isErr()', (t) => {
-  t.false(Result.of(0).isErr())
-  t.true(Result.of(new Error('foo')).isErr())
-  t.true(Err.isErr(Result.of(new Error('foo'))))
-  t.false(Ok.isOk(Result.of(new Error('foo'))))
+  t.false(Ok(0).isErr())
+  t.true(Err(new Error('foo')).isErr())
+  t.true(Err.isErr(Err(new Error('foo'))))
+  t.false(Ok.isOk(Err(new Error('foo'))))
 })
 
 test('map(fn)', (t) => {
-  t.is(Result.Ok.of(1).map((a) => a + 1).unwrapOr(100), 2)
-  t.is(Result.Err.of(1).map((a) => a + 1).unwrapOr(100), 100)
+  t.is(Result.Ok(1).map((a) => a + 1).unwrapOr(100), 2)
+  t.is(Result.Err(1).map((a) => a + 1).unwrapOr(100), 100)
 })
 
 test('mapErr(fn)', (t) => {
-  t.deepEqual(Result.Ok.of(2).mapErr((e) => 3), Ok.of(2))
-  t.deepEqual(Result.Err.of(2).mapErr((e) => 3), Err.of(3))
-  t.deepEqual(Result.of(2).map((e) => 4).mapErr((e) => 5), Ok.of(4))
+  t.is(Result.Ok(2).mapErr((e) => 3).unwrap(), 2)
+  t.is(Result.Err(2).mapErr((e) => 3).unwrapErr(), 3)
+  t.is(Ok(2).map((e) => 4).mapErr((e) => 5).unwrap(), 4)
+})
+
+test('bimap(f, g)', (t) => {
+  const foo = (x) => x * x
+  const bar = (y) => y + y
+
+  t.is(Ok(4).bimap(foo, bar).unwrap(), 16)
+  t.is(Err(4).bimap(foo, bar).unwrapErr(), 8)
 })
 
 test('iter()', (t) => {
-  t.is(Result.of(1).iter().next().value, 1, 'Ok::iter() return iterable over single item')
-  t.is(Result.of(new Error('')).iter().next().done, true, 'Err::iter() return done iterable')
+  t.is(Ok(1).iter().next().value, 1, 'Ok::iter() return iterable over single item')
+  t.is(Err(new Error('')).iter().next().done, true, 'Err::iter() return done iterable')
 })
 
 test('and(result)', (t) => {
-  let x = Result.of(2)
-  let y = Result.of(new Error('foo'))
+  let x = Ok(2)
+  let y = Err('foo')
 
-  t.is(x.and(y), y)
+  t.is(x.and(y).unwrapErr(), 'foo')
 
-  x = Result.of(new Error('bar'))
-  y = Result.of(2)
-  t.is(x.and(y), x)
+  x = Err('bar')
+  y = Ok(2)
+  t.is(x.and(y).unwrapErr(), 'bar')
 
-  x = Result.of(1)
-  y = Result.of(2)
-  t.is(x.and(y), y)
+  x = Ok(1)
+  y = Ok(2)
+  t.is(x.and(y).unwrap(), 2)
 })
 
 test('andThen(fn)', (t) => {
-  const sq = (x) => Ok.of(x * x)
-  const err = (x) => Err.of(x)
+  const sq = (x) => Ok(x * x)
+  const err = (x) => Err(x)
 
-  t.deepEqual(Result.of(2).andThen(sq).andThen(sq), Ok.of(16))
-  t.deepEqual(Result.of(2).andThen(sq).andThen(err), Err.of(4))
-  t.deepEqual(Ok.of(2).andThen(err).andThen(sq), Err.of(2))
-  t.deepEqual(Err.of(3).andThen(sq).andThen(sq), Err.of(3))
+  t.is(Ok(2).andThen(sq).andThen(sq).unwrap(), 16)
+  t.is(Ok(2).andThen(sq).andThen(err).unwrapErr(), 4)
+  t.is(Ok(2).andThen(err).andThen(sq).unwrapErr(), 2)
+  t.is(Err(3).andThen(sq).andThen(sq).unwrapErr(), 3)
 })
 
 test('or(result)', (t) => {
   let x, y
 
-  x = Ok.of(2)
-  y = Err.of('late error')
-  t.is(x.or(y), x)
+  x = Ok(2)
+  y = Err('late error')
+  t.is(x.or(y).unwrap(), 2)
 
-  x = Err.of('early error')
-  y = Ok.of(2)
-  t.is(x.or(y), y)
+  x = Err('early error')
+  y = Ok(2)
+  t.is(x.or(y).unwrap(), 2)
 
-  x = Err.of('not a 2')
-  y = Err.of('late error')
-  t.is(x.or(y), y)
+  x = Err('not a 2')
+  y = Err('late error')
+  t.is(x.or(y).unwrapErr(), 'late error')
 
-  x = Ok.of(2)
-  y = Ok.of(100)
-  t.is(x.or(y), x)
+  x = Ok(2)
+  y = Ok(100)
+  t.is(x.or(y).unwrap(), 2)
 })
 
 test('orElse(fn)', (t) => {
-  const sq = (x) => Ok.of(x * x)
-  const err = (x) => Err.of(x)
+  const sq = (x) => Ok(x * x)
+  const err = (x) => Err(x)
 
-  t.deepEqual(Ok.of(2).orElse(sq).orElse(sq), Ok.of(2))
-  t.deepEqual(Ok.of(2).orElse(err).orElse(sq), Ok.of(2))
-  t.deepEqual(Err.of(3).orElse(sq).orElse(err), Ok.of(9))
-  t.deepEqual(Err.of(3).orElse(err).orElse(err), Err.of(3))
+  t.is(Ok(2).orElse(sq).orElse(sq).unwrap(), 2)
+  t.is(Ok(2).orElse(err).orElse(sq).unwrap(), 2)
+  t.is(Err(3).orElse(sq).orElse(err).unwrap(), 9)
+  t.is(Err(3).orElse(err).orElse(err).unwrapErr(), 3)
 })
 
 test('unwrapOr(value)', (t) => {
-  t.is(Result.Ok.of(12).unwrapOr(1), 12)
-  t.is(Result.Err.of('foo').unwrapOr(19), 19)
+  t.is(Result.Ok(12).unwrapOr(1), 12)
+  t.is(Result.Err('foo').unwrapOr(19), 19)
 })
 
 test('unwrapOrElse(fn)', (t) => {
   const count = (x) => x.length
 
-  t.is(Ok.of(2).unwrapOrElse(count), 2)
-  t.is(Err.of('foo').unwrapOrElse(count), 3)
+  t.is(Ok(2).unwrapOrElse(count), 2)
+  t.is(Err('foo').unwrapOrElse(count), 3)
 })
 
 test('unwrap()', (t) => {
   t.throws(() => {
-    Err.of('emergency failure').unwrap()
+    Err('emergency failure').unwrap()
   }, /emergency failure/)
 
   t.notThrows(() => {
-    t.is(Ok.of(2).unwrap(), 2)
+    t.is(Ok(2).unwrap(), 2)
   })
 })
 
 test('unwrapErr()', (t) => {
   t.throws(() => {
-    Ok.of(2).unwrapErr()
+    Ok(2).unwrapErr()
   }, /2/)
 
   t.notThrows(() => {
-    t.is(Err.of(10).unwrapErr(), 10)
+    t.is(Err(10).unwrapErr(), 10)
   })
 })
 
 test('expect(msg)', (t) => {
   t.throws(() => {
-    Err.of('failure').expect('testing expect')
+    Err('failure').expect('testing expect')
   }, /testing expect/)
 
   t.notThrows(() => {
-    t.is(Ok.of(12).expect('not throws'), 12)
+    t.is(Ok(12).expect('not throws'), 12)
   })
 })
 
 test('expectErr(msg)', (t) => {
   t.throws(() => {
-    Ok.of(12).expectErr('testing expectErr')
+    Ok(12).expectErr('testing expectErr')
   }, /testing expectErr: 12/)
 
   t.notThrows(() => {
-    t.is(Err.of(12).expectErr('not throws'), 12)
+    t.is(Err(12).expectErr('not throws'), 12)
   })
 })
 
-// test('thenable', async (t) => {
-//   t.is(await Ok.of(12), 12)
-//   try {
-//     await Err.of('throws')
-//   }
-//   catch (error) {
-//     t.is(error, 'throws')
-//   }
-// })
-
-// test('catcheable', (t) => {
-//   t.deepEqual(Ok.of(1).catch(() => 2), Ok.of(1))
-//   t.is(Err.of(1).catch(() => 2), 2)
-// })
-
 test('promise()', async (t) => {
-  t.true(Ok.of(1).promise() instanceof Promise)
+  t.true(Ok(1).promise() instanceof Promise)
   // t.true(Err.from(2).promise() instanceof Promise)
 
   t.notThrows(async () => {
-    t.is(await Ok.of(1).promise(), 1)
+    t.is(await Ok(1).promise(), 1)
   })
 
   try {
-    await Err.of(2).promise()
+    await Err(2).promise()
   }
   catch (error) {
     t.is(error, 2)
@@ -235,30 +196,23 @@ test('promise()', async (t) => {
 })
 
 test('swap()', (t) => {
-  t.deepEqual(Ok.of(1).swap(), Err.of(1))
-  t.deepEqual(Err.of(2).swap(), Ok.of(2))
+  t.is(Ok(1).swap().unwrapErr(), 1)
+  t.is(Err(2).swap().unwrap(), 2)
 })
 
-test('combine .and() and .of() to make all() function', (t) => {
-  const all = (ls) => ls.reduce((p, c) => p.and(c), Ok.of(true))
+test('combine .and() and () to make all() function', (t) => {
+  const all = (ls) => ls.reduce((p, c) => p.and(c), Ok(true))
 
-  t.deepEqual(all([Ok.of(1), Ok.of(2)]), Ok.of(2))
+  t.is(all([Ok(1), Ok(2)]).unwrap(), 2)
 })
 
 test('extract()', (t) => {
-  t.deepEqual(Ok.of(1).extract(), [1])
-  t.deepEqual(Err.of(1).extract(), [])
+  t.deepEqual(Ok(1).extract(), [1])
+  t.deepEqual(Err(1).extract(), [])
 })
 
 test('extractErr()', (t) => {
-  t.deepEqual(Ok.of(1).extractErr(), [])
-  t.deepEqual(Err.of(1).extractErr(), [1])
+  t.deepEqual(Ok(1).extractErr(), [])
+  t.deepEqual(Err(1).extractErr(), [1])
 })
 
-test('bimap(f, g)', (t) => {
-  const foo = (x) => x * x
-  const bar = (y) => y + y
-
-  t.deepEqual(Ok.of(4).bimap(foo, bar), Ok.of(16))
-  t.deepEqual(Err.of(4).bimap(foo, bar), Err.of(8))
-})
