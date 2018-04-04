e54c2d9894e9510a4be90b6224015d00a33c024e
Sergey Sova <mail@sergeysova.com>
2018-04-04 13:45:03 +0300

    feat: add equals method

--- test/test.js
+++ test/test.js
@@ -14,16 +14,16 @@ test('no constructor of Result', (t) => {
   t.true(typeof Result === 'object')
 })
 
-test('Ok()', (t) => {
+test('Ok :: Result f, b => a -> f a b', (t) => {
   t.is(Result.Ok(1).unwrap(), 1)
 })
 
-test('Err()', (t) => {
+test('Err :: Result f, a => b -> f a b', (t) => {
   t.is(Result.Err(1).unwrapErr(), 1)
 })
 
 
-test('Result.isResult(target)', (t) => {
+test('isResult :: Result f => f a b -> Boolean', (t) => {
   t.true(Result.isResult(Ok(1)))
   t.true(Result.isResult(Err(1)))
 
@@ -34,32 +34,40 @@ test('Result.isResult(target)', (t) => {
   t.false(Result.isResult(Result.Err))
 })
 
-test('isOk()', (t) => {
+test('isOk :: Result f => f a b ~> Boolean', (t) => {
   t.true(Ok(0).isOk())
   t.false(Err(new Error('foo')).isOk())
   t.true(Ok.isOk(Ok(1)))
   t.false(Err.isErr(Ok(1)))
 })
 
-test('isErr()', (t) => {
+test('isErr :: Result f => f a b ~> Boolean', (t) => {
   t.false(Ok(0).isErr())
   t.true(Err(new Error('foo')).isErr())
   t.true(Err.isErr(Err(new Error('foo'))))
   t.false(Ok.isOk(Err(new Error('foo'))))
 })
 
-test('map(value => value)', (t) => {
+test('equals :: Result f => f ~> f -> Boolean', (t) => {
+  t.true(Ok(1).equals(Ok(1)))
+  t.false(Ok(2).equals(Ok(1)))
+  t.false(Err(1).equals(Ok(1)))
+  t.false(Err(1).equals(Err(2)))
+  t.true(Err(1).equals(Err(1)))
+})
+
+test('map :: Result f => f a b ~> (a -> q) -> f q', (t) => {
   t.is(Result.Ok(1).map((a) => a + 1).unwrapOr(100), 2)
   t.is(Result.Err(1).map((a) => a + 1).unwrapOr(100), 100)
 })
 
-test('mapErr(error => error)', (t) => {
+test('mapErr :: Result f => f a b ~> (b -> z) -> f z', (t) => {
   t.is(Result.Ok(2).mapErr((e) => 3).unwrap(), 2)
   t.is(Result.Err(2).mapErr((e) => 3).unwrapErr(), 3)
   t.is(Ok(2).map((e) => 4).mapErr((e) => 5).unwrap(), 4)
 })
 
-test('bimap(f, g)', (t) => {
+test('bimap :: Result f => f a b ~> (a -> q, b -> z) -> f q z', (t) => {
   const foo = (x) => x * x
   const bar = (y) => y + y
 
@@ -67,7 +75,7 @@ test('bimap(f, g)', (t) => {
   t.is(Err(4).bimap(foo, bar).unwrapErr(), 8)
 })
 
-test('chain(value => result)', (t) => {
+test('chain :: Result f => f a b ~> (a -> f q b) -> f q b', (t) => {
   t.is(Ok(1).chain((value) => Ok(value + 1)).unwrap(), 2)
   t.is(Ok(1).chain((value) => Err(value - 1)).unwrapErr(), 0)
 
@@ -75,7 +83,7 @@ test('chain(value => result)', (t) => {
   t.is(Err(1).chain((value) => Err(value - 1)).unwrapErr(), 1)
 })
 
-test('chainErr(error => result)', (t) => {
+test('chainErr: :: Result f => f a b ~> (b -> f a z) -> f a z', (t) => {
   t.is(Ok(1).chainErr((value) => Ok(value + 1)).unwrap(), 1)
   t.is(Ok(1).chainErr((value) => Err(value - 1)).unwrap(), 1)
 
@@ -88,7 +96,7 @@ test('iter()', (t) => {
   t.is(Err(new Error('')).iter().next().done, true, 'Err::iter() return done iterable')
 })
 
-test('and(result)', (t) => {
+test('and :: Result f => f a b ~> f q b -> f q b', (t) => {
   let x = Ok(2)
   let y = Err('foo')
 
@@ -103,7 +111,8 @@ test('and(result)', (t) => {
   t.is(x.and(y).unwrap(), 2)
 })
 
-test('andThen(fn)', (t) => {
+test('andThen :: Result f => f a b ~> (a -> f q b) -> f q b', (t) => {
+  // :: Result f, b => a -> f a b
   const sq = (x) => Ok(x * x)
   const err = (x) => Err(x)
 
@@ -113,27 +122,28 @@ test('andThen(fn)', (t) => {
   t.is(Err(3).andThen(sq).andThen(sq).unwrapErr(), 3)
 })
 
-test('or(result)', (t) => {
+test('or :: Result f => f a b ~> f a z -> f a z', (t) => {
   let x, y
 
   x = Ok(2)
-  y = Err('late error')
+  y = Err(10)
   t.is(x.or(y).unwrap(), 2)
 
-  x = Err('early error')
+  x = Err(10)
   y = Ok(2)
   t.is(x.or(y).unwrap(), 2)
 
-  x = Err('not a 2')
-  y = Err('late error')
-  t.is(x.or(y).unwrapErr(), 'late error')
+  x = Err(2)
+  y = Err(10)
+  t.is(x.or(y).unwrapErr(), 10)
 
   x = Ok(2)
   y = Ok(100)
   t.is(x.or(y).unwrap(), 2)
 })
 
-test('orElse(fn)', (t) => {
+test('orElse :: Result f => f a b ~> (b -> f a z) -> f a z', (t) => {
+  // :: Result f, b => a -> f a b
   const sq = (x) => Ok(x * x)
   const err = (x) => Err(x)
 
@@ -143,19 +153,7 @@ test('orElse(fn)', (t) => {
   t.is(Err(3).orElse(err).orElse(err).unwrapErr(), 3)
 })
 
-test('unwrapOr(value)', (t) => {
-  t.is(Result.Ok(12).unwrapOr(1), 12)
-  t.is(Result.Err('foo').unwrapOr(19), 19)
-})
-
-test('unwrapOrElse(fn)', (t) => {
-  const count = (x) => x.length
-
-  t.is(Ok(2).unwrapOrElse(count), 2)
-  t.is(Err('foo').unwrapOrElse(count), 3)
-})
-
-test('unwrap()', (t) => {
+test('unwrap :: Result f => f a b ~> a!', (t) => {
   t.throws(() => {
     Err('emergency failure').unwrap()
   }, /emergency failure/)
@@ -165,7 +163,20 @@ test('unwrap()', (t) => {
   })
 })
 
-test('unwrapErr()', (t) => {
+
+test('unwrapOr :: Result f => f a b ~> a -> a', (t) => {
+  t.is(Result.Ok(12).unwrapOr(1), 12)
+  t.is(Result.Err('foo').unwrapOr(19), 19)
+})
+
+test('unwrapOrElse :: Result f => f a b ~> (a -> a) -> a', (t) => {
+  const count = (x) => x.length
+
+  t.is(Ok(2).unwrapOrElse(count), 2)
+  t.is(Err('foo').unwrapOrElse(count), 3)
+})
+
+test('unwrapErr :: Result f => f a b ~> b!', (t) => {
   t.throws(() => {
     Ok(2).unwrapErr()
   }, /2/)
@@ -175,7 +186,7 @@ test('unwrapErr()', (t) => {
   })
 })
 
-test('expect(msg)', (t) => {
+test('expect :: Result f => f a b ~> String -> a!', (t) => {
   t.throws(() => {
     Err('failure').expect('testing expect')
   }, /testing expect/)
@@ -185,7 +196,7 @@ test('expect(msg)', (t) => {
   })
 })
 
-test('expectErr(msg)', (t) => {
+test('expectErr :: Result f => f a b ~> String -> b!', (t) => {
   t.throws(() => {
     Ok(12).expectErr('testing expectErr')
   }, /testing expectErr: 12/)
@@ -195,7 +206,7 @@ test('expectErr(msg)', (t) => {
   })
 })
 
-test('promise()', async (t) => {
+test('promise :: Result f, Promise p => f a b ~> p a b', async (t) => {
   t.true(Ok(1).promise() instanceof Promise)
   // t.true(Err.from(2).promise() instanceof Promise)
 
@@ -211,7 +222,7 @@ test('promise()', async (t) => {
   }
 })
 
-test('swap()', (t) => {
+test('swap :: Result f => f a b ~> f b a', (t) => {
   t.is(Ok(1).swap().unwrapErr(), 1)
   t.is(Err(2).swap().unwrap(), 2)
   t.is(Ok(1).swap().swap().unwrap(), 1)
@@ -219,17 +230,18 @@ test('swap()', (t) => {
 })
 
 test('combine .and() and () to make all() function', (t) => {
+  // all :: Result f, Array l => l (f a b) -> f a b
   const all = (ls) => ls.reduce((p, c) => p.and(c), Ok(true))
 
   t.is(all([Ok(1), Ok(2)]).unwrap(), 2)
 })
 
-test('extract()', (t) => {
+test('extract :: Result f, Tuple t => Result f a b ~> t a', (t) => {
   t.deepEqual(Ok(1).extract(), [1])
   t.deepEqual(Err(1).extract(), [])
 })
 
-test('extractErr()', (t) => {
+test('extract :: Result f, Tuple t => Result f a b ~> t b', (t) => {
   t.deepEqual(Ok(1).extractErr(), [])
   t.deepEqual(Err(1).extractErr(), [1])
 })
